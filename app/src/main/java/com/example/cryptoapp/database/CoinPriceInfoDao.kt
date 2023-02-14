ba8f1b8b1b51cdf0cee46e770f31e7514c7cd1b7
andrew <andrew.vorobiev.work@gmail.com>
2023-02-14 14:55:53 +0300

    Создание CoinPriceInfoDao

---
+++ app/src/main/java/com/example/cryptoapp/database/CoinPriceInfoDao.kt
@@ -0,0 +1,26 @@
+package com.example.cryptoapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.cryptoapp.pojo.CoinPriceInfo
+
+@Dao
+interface CoinPriceInfoDao {
+
+    // ORDER BY lastUpdate - сортировка по последней дате обновления
+    // Этот метод нам понадобиться, чтобы выводить список в RecyclerView
+    @Query("SELECT * FROM full_price_list ORDER BY lastUpdate")
+    fun getPriceList() : LiveData<List<CoinPriceInfo>>
+
+    @Query("SELECT * FROM full_price_list WHERE fromSymbol == :fSym LIMIT 1")
+    fun getPriceInfoAboutCoin(fSym: String): LiveData<CoinPriceInfo>
+
+
+    // OnConflictStrategy.REPLACE - Когда будут приходить новые данные, они будут заменяться
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertPriceList(priceList: List<CoinPriceInfo>)
+}
+
